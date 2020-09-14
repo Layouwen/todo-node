1bf9c1e9d467d4f6b5c6443c2693bd7d32e7ff40
liangyouwen <layouwen@gmail.com>
2020-09-15 00:40:18 +0800

    update version

--- cli.js
+++ cli.js
@@ -1,8 +1,10 @@
+#!/usr/bin/env node
 const program = require("commander")
 const api = require("./index.js")
+const pkg = require("./package.json")
 
 program
-  .option("-x, --xxx", "what the x")
+  .version(pkg.version)
 program
   .command("add")
   .description("add a task")
