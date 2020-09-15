f50641c8c11a17efd10dc13a90333b091d00dd0f
liangyouwen <layouwen@gmail.com>
2020-09-15 22:17:16 +0800

    完成单元测试

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,8 +1,40 @@
 const fs = jest.genMockFromModule("fs") // 声明这是jest的假模块
+const _fs = jest.requireActual("fs") // 引用真实的fs
 
-fs.x = () => {
-  console.log("1")
-  return "xxx"
+Object.assign(fs, _fs) // 复制真实的fs给假的fs
+
+let readMocks = {}
+
+fs.setReadFileMock = (path, error, data) => {
+  readMocks[path] = [error, data]
+}
+
+fs.readFile = (path, options, callback) => {
+  if (callback === undefined) callback = options
+  if (path in readMocks) {
+    callback(...readMocks[path])
+  } else {
+    _fs.readFile(path, options, callback)
+  }
+}
+
+let writeMocks = {}
+
+fs.setWriteFileMock = (path, fn) => {
+  writeMocks[path] = fn
+}
+
+fs.writeFile = (path, data, options, callback) => {
+  if (path in writeMocks) {
+    writeMocks[path](path, data, options, callback)
+  } else {
+    _fs.writeFile(path, data, options, callback)
+  }
+}
+
+fs.clearMocks = () => {
+  readMocks = {}
+  writeMocks = {}
 }
 
 module.exports = fs
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -1,8 +1,29 @@
 const db = require("../db.js")
+const fs = require("fs")
 jest.mock("fs")
 
 describe("db", () => { // 描述
-  it("can read", () => {
-    expect(fs.x()).toBe("xxx") // 期待它的返回值为 xxx
+  afterEach(() => {
+    fs.clearMocks() // 每次测试先删除mock
+  })
+
+  it("can read", async () => {
+    const data = [{title: "hi", done: true}]
+    fs.setReadFileMock("/hi", null, JSON.stringify(data)) // 创建假数据
+    const list = await db.read("/hi")
+    expect(list).toStrictEqual(data)
+  })
+
+  it("can write", async () => {
+    let fakeFile
+
+    fs.setWriteFileMock("/yyy", (path, data, callback) => {
+      fakeFile = data
+      callback(null)
+    })
+
+    const list = [{title: "看星星", done: true}, {title: "看电影", done: true}]
+    await db.write(list, "/yyy")
+    expect(fakeFile).toBe(JSON.stringify(list) + "\n")
   })
 })
